c6792c2940a1d471499e66090a3abd25a2314260
HamzaHamani <hamzahamani2004@gmail.com>
2024-01-04 18:57:58 +0100

    CONNECTED WITH TMDB API

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
 import Image from "next/image";
+import { cache } from "react";
 
-export default function Home() {
-  return <main>hello</main>;
+export default async function Home() {
+
+  const res = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: USEENV VALUE HERE,
+      },
+    }
+  );
+  const data = await res.json();
+  console.log(data.results[0]);
+  // console.log(data);
+
+  return (
+    <main>
+      {data.results?.map((e: any) => (
+        <p key={e.id}>{e.original_title}</p>
+      ))}
+    </main>
+  );
 }
